51f84456fb48c1cef03e910052949609b5047292
fairguy00 <thbusae@gmail.com>
2021-11-28 15:47:14 +0900

    Mocking위한 단위테스트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <todo-title></todo-title>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import TodoTitle from './components/TodoTitle.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    TodoTitle
   }
 }
 </script>
---
+++ src/components/TodoTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{todo.title}}
+    </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    // Mock - 모의
+    // Mocking  위한 컴포넌트 생성
+    data(){
+        return{
+            todo:{}
+        }
+    },
+    created() {
+        this.fetchTodo()
+    },
+    methods:{
+        async fetchTodo(){
+            const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            
+            this.todo = data
+        }
+    }
+}
+</script>
